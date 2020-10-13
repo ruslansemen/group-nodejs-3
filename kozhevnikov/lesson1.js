3b68e3a08150bd6b77cbc4c73d1035d2742ef74b
KostasKA2020 <konstakozh@yandex.ru>
2020-10-13 17:52:12 +0400

    Lesson_1

---
+++ kozhevnikov/lesson1.js
@@ -0,0 +1,12 @@
+const chalk = require('chalk')
+const beepbeep = require('beepbeep')
+
+const log = console.log
+log(chalk.white.bgRed.bold('Hello') + chalk.underline.bgBlue(' World') + chalk.red('!'))
+log(`
+CPU: ${chalk.red('90%')}
+RAM: ${chalk.green('40%')}
+DISK: ${chalk.yellow('70%')}
+`)
+
+beepbeep(3, 1000)

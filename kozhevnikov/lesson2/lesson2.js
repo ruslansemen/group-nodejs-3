aabd0816fb0bd626b7848a84976d9bc8dbddde84
KostasKA2020 <konstakozh@yandex.ru>
2020-10-16 15:50:08 +0400

    Lesson_2

--- kozhevnikov/lesson2/lesson2.js
+++ kozhevnikov/lesson2/lesson2.js
@@ -1,14 +1,29 @@
 const Promise = require("bluebird");
 const randomNumber = require("random-number-csprng");
 const readlineSync = require('readline-sync');
+let readline = require('readline');
 
-Promise.try(function() {
-    return randomNumber(0, 1);
-}).then(function(number) {
-    console.log("Your random number:", number);
-}).catch({code: "RandomGenerationError"}, function(err) {
-    console.log("Something went wrong!");
+// Promise.try(function() {
+//     return randomNumber(0, 1);
+// }).then(function(number) {
+//     console.log("Your random number:", number);
+// }).catch({code: "RandomGenerationError"}, function(err) {
+//     console.log("Something went wrong!");
+// });
+
+
+let rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'Ввведите 0 ли 1 >'
+});
+rl.prompt();
+rl.on('line', (input) => {
+    input = input.toLowerCase();
+    console.log(input);
+    rl.close();
 });
 
-const userNamber = readlineSync.question('Введите 0 или 1 ');
-console.log('Вы ввели ', userNamber);
+// const userName = readlineSync.question('Как тебя зовут? ', 'utf-8');
+// const userNamber = readlineSync.question('Hit 1...5 key: ', {limit: '$<1-5>'});
+// console.log('Вы ввели ', userNamber);

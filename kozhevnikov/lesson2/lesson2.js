aa7b7f3eeef41209e9c9ffef4cb7539e6b5028b4
KostasKA2020 <konstakozh@yandex.ru>
2020-10-16 12:56:19 +0400

    Lesson_2

--- kozhevnikov/lesson2/lesson2.js
+++ kozhevnikov/lesson2/lesson2.js
@@ -1,5 +1,6 @@
-var Promise = require("bluebird");
-var randomNumber = require("random-number-csprng");
+const Promise = require("bluebird");
+const randomNumber = require("random-number-csprng");
+const readlineSync = require('readline-sync');
 
 Promise.try(function() {
     return randomNumber(0, 1);
@@ -8,3 +9,6 @@ Promise.try(function() {
 }).catch({code: "RandomGenerationError"}, function(err) {
     console.log("Something went wrong!");
 });
+
+const userNamber = readlineSync.question('Введите 0 или 1 ');
+console.log('Вы ввели ', userNamber);

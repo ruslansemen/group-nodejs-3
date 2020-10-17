26a97975a1120e598e268012bf335d642980e2f8
KostaKA2020 <konstakozh@yandex.ru>
2020-10-17 21:11:50 +0400

    lesson2

--- kozhevnikov/lesson2/lesson2.js
+++ kozhevnikov/lesson2/lesson2.js
@@ -1,16 +1,28 @@
 const random = require('random')
-const readlineSync = require('readline-sync');
+const readlineSync = require('readline-sync')
 const chalk = require('chalk')
 
 
-console.log('\x1Bc');
+console.log('\x1Bc')
+let countUser = 0
+let countMashine =0
 while (true) {
-    input  = readlineSync . keyIn ( ' Hit 0 or 1 key:  ' , { limit : ' $ <0-1> ' } ) ;
-    console.log(' Вы ввели ', input);
-    console.log(' Машина выбрала', random.int(min = 0, max = 1));
+    inputUser  = readlineSync . keyIn ( ' Hit 0 or 1 key:  ' , { limit : ' $ <0-1> ' } )
+    console.log(' Вы ввели ', inputUser)
+    inputMashine = random.int(min = 0, max = 1)
+    console.log(' Компьютер выбрал', inputMashine)
+    if (inputUser == inputMashine) {
+        countUser++
+        console.log(' Ура! Вы выиграли')
+    } else {
+        countMashine++
+        console.log(' Выиграл компьютер')
+    }
+
     boolYes = readlineSync.keyInYNStrict([' Continue the game? '])
     if (!boolYes) {
-        break;
+        console.log('\x1Bc'+' Game over')
+        break
     }
 }
-console.log(' Вы ввели ', boolYes);
+console.log(' Счет в партии ' + countUser + ' : ' + countMashine)

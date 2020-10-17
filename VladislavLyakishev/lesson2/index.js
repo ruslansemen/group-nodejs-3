46edee62990c53322c9ac5d3db9c08d468453b10
Владислав Лякишев <vlad169@yandex.ru>
2020-10-17 20:10:51 +0300

    Create read and write result in file

--- VladislavLyakishev/lesson2/index.js
+++ VladislavLyakishev/lesson2/index.js
@@ -1,8 +1,16 @@
 const readline = require('readline')
 const colors = require('colors');
+const fs = require('fs')
+const path = require('path')
    
 class Game {
 
+    result = {
+        name: 'Player',
+        count: 0,
+        round: 0
+    }
+    readFile = []
     rl = null
     start = false
     randomNumber = 0
@@ -16,6 +24,15 @@ class Game {
             input: process.stdin,
             output: process.output
         })
+        fs.readFile(path.join(__dirname, 'db.json'), 'utf8', (err, data) => {
+            if (err) {
+                throw err
+            }
+            if (data){
+                this.readFile = JSON.parse(data)
+            }
+            
+        })
     }
     init(){
         console.log(this.hello);
@@ -25,24 +42,41 @@ class Game {
                 this.createRandomNumber()
                 console.log('Компьютер подкинул монетку, введите ответ..0 или 1');
             }
-            if (this.start && +cmd > 0) {
+            if (this.start && +cmd >= 0) {
                 if (+cmd === this.randomNumber) {
-                    console.log('Поздравляю, вы угадали. Введите start, чтобы начать новый раунд или exit для выхода из игры'.green)
-                    this.start = false
+                    this.endRound(1)
                 } else {
-                    console.log('К сожалению вы не угадали. Введите start, чтобы начать новый раунд или exit для выхода из игры'.red)
-                    this.start = false
+                    this.endRound(0)
                 }
             }
         })
     }
     close(){
-        console.log('Bye, bye'.green);
+        console.log('Пока пока, результат игры записан'.green);
+        this.writeResult()
         this.rl.close()
     }
     createRandomNumber(){
         this.start = true
         return this.randomNumber = Math.floor(Math.random() * 2)
+    }
+    endRound(isFinall){
+        this.start = false
+        if (isFinall) {
+            console.log('Поздравляю, вы угадали. Введите start, чтобы начать новый раунд или exit для выхода из игры'.green)
+            this.result.count++
+            this.result.round++
+            
+        } else {
+            console.log('К сожалению вы не угадали. Введите start, чтобы начать новый раунд или exit для выхода из игры'.red)
+            this.result.round++
+        }
+    }
+    writeResult(){
+        this.readFile.push(this.result)
+        fs.writeFileSync(path.join(__dirname, 'db.json'), JSON.stringify(this.readFile), (err) => {
+            throw err
+        })
     }
 }
 

df96b72c9e029a7af6056431ccfe3ffcbd14532a
Alexander <lotos_toi@mail.ru>
2020-10-19 20:54:51 +1000

    lesson_2 is ready

--- Alexander Plotnikov/lesson_2/index.js
+++ Alexander Plotnikov/lesson_2/index.js
@@ -1,9 +1,11 @@
-const readline = require('readline');
-const chalk = require('chalk');
-const fs = require('fs')
+const readline = require('readline')
+const chalk = require('chalk')
 const path = require('path')
 
+const { statistic, round, start, task, error, writeToLog } = require('./src/functions')
+
 let arg = process.argv.filter(e => /\-\-/.test(e))[0]
+
 const link = path.join(__dirname, arg ? arg.replace(/\-\-/, '') : 'log.json')
 
 const rl = readline.createInterface({
@@ -19,10 +21,18 @@ rl.on('line', async (input) => {
         rl.close()
     }
     else if (input === 'statistic') {
-        await statistic(link)
+        try {
+            await statistic(link)
+        } catch (error) {
+            console.log(chalk.red(error));
+        }
     }
     else if (+input === 1 || +input === 2) {
-        await writeToLog(link, round(+input))
+        try {
+            await writeToLog(link, round(+input))
+        } catch (error) {
+            console.log(chalk.red(error));
+        }
         task()
     } else {
         error()
@@ -30,63 +40,3 @@ rl.on('line', async (input) => {
 })
 
 
-
-function start() { console.log(chalk.yellow('************* Welcome to the game heads and tails !!! ***********************')) }
-function task() { console.log(chalk.magenta(`Input two numbers: 1 or 2. If you want to exit the game to input "exit"`)) }
-function error() { console.log(chalk.red(`You inputed uncorrect value... You need to input 1 or 2... Try again `)) }
-function rnd(min, max) {
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    return Math.floor(Math.random() * (max - min + 1)) + min
-}
-function round(numder) {
-    let result = rnd(1, 2) === numder
-    result ? console.log(chalk.green('You win!')) : console.log(chalk.red('You loose...'))
-    return result
-}
-
-
-function read(link) {
-    return new Promise((res, rej) => {
-        fs.readFile(link, 'utf8', (err, data) => {
-            if (err) rej(err)
-            res(data)
-        })
-    })
-}
-
-
-
-function write(link, data) {
-    return new Promise((res, rej) => {
-        fs.writeFile(link, data, 'utf8', (err) => {
-            err ? rej(err) : res()
-        })
-    })
-}
-
-async function writeToLog(link, result) {
-
-    let log = await read(link)
-    log = log ? JSON.parse(log) : []
-
-    let newRecoding = {
-        numberOfGames: log.length,
-        result: result ? "win" : "loose",
-    }
-
-    log.push(newRecoding)
-
-    await write(link, JSON.stringify(log))
-
-}
-
-async function statistic(link) {
-    let log = await read(link)
-    log = log ? JSON.parse(log) : []
-
-    console.log("Total - ", log.length)
-    console.log("Total of wins - ", log.reduce((accum, game) => game.result === "win" && accum + 1, 0))
-    console.log("Total of looses - ", allQuantity - allWin)
-    console.log("Ratio win/loose - ", allWin / (allLoose || 1))
-}

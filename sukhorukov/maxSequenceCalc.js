0bdedaa9077d57c153723ebf4eca432dfd15995e
rusla <suhrugen@gmail.com>
2020-10-20 01:12:48 +0700

    оптимизация модуля

--- sukhorukov/maxSequenceCalc.js
+++ sukhorukov/maxSequenceCalc.js
@@ -2,17 +2,15 @@ maxSequence = (arr, win) => {
     let sequence = []
     let count = 0
 
-    if (win) {
-        for (let item of arr) {
+    for (let item of arr) {
+        if (win) {
             if (item.win) {
                 count++
             } else {
                 sequence.push(count)
                 count = 0
             } 
-        } 
-    } else {
-        for (let item of arr) {
+        } else {
             if (!item.win) {
                 count++
             } else {

6a67a4f8b67a78f1cae1b7219037296c4e28bc76
KostasKA2020 <konstakozh@yandex.ru>
2020-10-20 23:42:23 +0400

    lesson3

--- kozhevnikov/lesson3/lesson3.js
+++ kozhevnikov/lesson3/lesson3.js
@@ -1,21 +1,29 @@
-const https = require('https')
+// const https = require('https')
+const request = require('request')
 
-https.get('https://rossaprimavera.ru', (result) => {
-    console.log(result.statusCode)
-    if (result.statusCode !==200) {
-        return
-    }
-    result.setEncoding ('utf8')
-
-    let contentData = ''
-    result.on('data', (chunk) => {
-        // console.log(chunk)
-        contentData += chunk
-    })
-    result.on('end', () => {
+request('https://rossaprimavera.ru', (err, response,body) => {
+    if(!err && response.statusCode === 200) {
         console.log('Все данные успешно получены')
-        console.log(contentData)
-    })
-}).on('error', (err) => (
-    console.log('Error', err)
-))
+        console.log(body)
+        console.log('Все данные успешно получены')
+    }
+})
+// https.get('https://rossaprimavera.ru', (result) => {
+//     console.log(result.statusCode)
+//     if (result.statusCode !==200) {
+//         return
+//     }
+//     result.setEncoding ('utf8')
+//
+//     let contentData = ''
+//     result.on('data', (chunk) => {
+//         // console.log(chunk)
+//         contentData += chunk
+//     })
+//     result.on('end', () => {
+//         console.log('Все данные успешно получены')
+//         console.log(contentData)
+//     })
+// }).on('error', (err) => (
+//     console.log('Error', err)
+// ))

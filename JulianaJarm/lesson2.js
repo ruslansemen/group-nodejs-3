fafb84691feba574f6c888a7871e5a6f1f7e9507
julianajarm <julianajarm@gmail.com>
2020-10-20 14:43:14 +0300

    game created, works fine

--- JulianaJarm/lesson2.js
+++ JulianaJarm/lesson2.js
@@ -1,30 +1,46 @@
-const {EventEmitter} = require('events')
-const minimistLib = require('minimist')
 const readline = require('readline')
 const fs = require('fs')
+const chalk = require('chalk');
 
-
-class Game extends EventEmitter {
-    constructor() {
-        super();
-    }
-
-}
-
-const argv = minimistLib(process.argv.slice(2), {
-    default: {
-        playerGuess: '',
-        win: false,
-    }
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
 })
 
-/*
-1) Написать консольную игру "Орел или решка", в которой надо будет
-угадывать выпадающее число (1 или 2).
-В качестве аргумента
-программа может принимать имя файла для логирования
-результатов каждой партии.
+function play(fileName) {
+    rl.on('line', (guess) => {
+        if (guess === 'e' || guess === 'exit') rl.close()
+        guess = parseInt(guess);
 
+        if (guess === 1 || guess === 2) {
+            const result = Math.floor(Math.random()* 2 + 1)
+            let win = false
+            if (guess === result) {
+                win = true
+                console.log('You win! :D')
+            } else {
+                console.log('You lose :(')
+            }
+            fs.appendFile(`${fileName}`, JSON.stringify(`${guess}: ${win},`), (error) => {
+                if(error) throw new Error;
+            })
+
+        } else {
+            console.log('Wrong input. You need to enter 1 or 2')
+        }
+    })
+}
+
+play('gameLog.json');
+
+
+
+
+
+
+
+
+/*
 2) Сделать программу-анализатор игровых логов. В качестве
 аргумента программа получает путь к файлу. Выведите игровую
 статистику: общее количество партий, количество выигранных /

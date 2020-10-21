39ee39ac85d9aaf9a17a756e139f207b9d6bbe21
KostasKA2020 <konstakozh@yandex.ru>
2020-10-21 09:15:37 +0400

    lesson3

--- kozhevnikov/lesson3/lesson3.js
+++ kozhevnikov/lesson3/lesson3.js
@@ -1,29 +1,46 @@
-// const https = require('https')
-const request = require('request')
+const tress = require('tress');
+const needle = require('needle');
+const cheerio = require('cheerio');
+const resolve = require('url').resolve;
+const fs = require('fs');
 
-request('https://rossaprimavera.ru', (err, response,body) => {
-    if(!err && response.statusCode === 200) {
-        console.log('Все данные успешно получены')
-        console.log(body)
-        console.log('Все данные успешно получены')
-    }
-})
-// https.get('https://rossaprimavera.ru', (result) => {
-//     console.log(result.statusCode)
-//     if (result.statusCode !==200) {
-//         return
-//     }
-//     result.setEncoding ('utf8')
-//
-//     let contentData = ''
-//     result.on('data', (chunk) => {
-//         // console.log(chunk)
-//         contentData += chunk
-//     })
-//     result.on('end', () => {
-//         console.log('Все данные успешно получены')
-//         console.log(contentData)
-//     })
-// }).on('error', (err) => (
-//     console.log('Error', err)
-// ))
+const URL = 'http://www.ferra.ru/ru/techlife/news/';
+const results = [];
+
+const q = tress(function(url, callback){
+    needle.get(url, function(err, res){
+        if (err) throw err;
+
+        // парсим DOM
+        var $ = cheerio.load(res.body);
+
+        //информация о новости
+        if($('.b_infopost').contents().eq(2).text().trim().slice(0, -1) !== 'Алексей Козлов'){
+            results.push({
+                title: $('h1').text(),
+                date: $('.b_infopost>.date').text(),
+                href: url,
+                size: $('.newsbody').text().length
+            });
+        }
+
+        //список новостей
+        $('.b_rewiev p>a').each(function() {
+            q.push($(this).attr('href'));
+        });
+
+        //паджинатор
+        $('.bpr_next>a').each(function() {
+            // не забываем привести относительный адрес ссылки к абсолютному
+            q.push(resolve(URL, $(this).attr('href')));
+        });
+
+        callback();
+    });
+}, 10); // запускаем 10 параллельных потоков
+
+q.drain = function(){
+    fs.writeFileSync('./data.json', JSON.stringify(results, null, 4));
+}
+
+q.push(URL);

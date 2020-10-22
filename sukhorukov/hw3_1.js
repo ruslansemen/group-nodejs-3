c5906d968d8fb88d51f9a3f735736a6a04e437a9
rusla <suhrugen@gmail.com>
2020-10-22 20:14:52 +0700

    pretier formated

--- sukhorukov/hw3_1.js
+++ sukhorukov/hw3_1.js
@@ -1,33 +1,33 @@
 const request = require('request')
 const cheerio = require('cheerio')
 const format = require('date-format')
-const {clear} = require('console')
-const {log} = require('console')
-const {table} = require('console')
+const { clear } = require('console')
+const { log } = require('console')
+const { table } = require('console')
 
 clear()
 log('\nplease, wait ...\n')
 
 request('https://distrowatch.com/', (err, response, body) => {
-    if (!err && response.statusCode === 200) {
-        const $ = cheerio.load(body)        
-        const top = []
-        
-        for (let i=0; i<10; i++) {  
-            const line = {}
+  if (!err && response.statusCode === 200) {
+    const $ = cheerio.load(body)
+    const top = []
 
-            line.place = +$('.phr1').eq(i).text()
-            line.name = $('.phr2').eq(i).text()
-            line.loadsPerDay = +$('.phr3').eq(i).text()
+    for (let i = 0; i < 10; i++) {
+      const line = {}
 
-            top[i] = line
-        }  
+      line.place = +$('.phr1').eq(i).text()
+      line.name = $('.phr2').eq(i).text()
+      line.loadsPerDay = +$('.phr3').eq(i).text()
 
-        clear()
-        log('\n OS TOP10 on', format('dd.MM.yyyy', new Date()))
-
-        table(top)
-    } else {
-        log('error: ', err)
+      top[i] = line
     }
+
+    clear()
+    log('\n OS TOP10 on', format('dd.MM.yyyy', new Date()))
+
+    table(top)
+  } else {
+    log('error: ', err)
+  }
 })
--- sukhorukov/hw3_2.js
+++ sukhorukov/hw3_2.js
@@ -1,28 +1,33 @@
 const readline = require('readline')
-const {clear} = require('console')
-const {log} = require('console')
+const { clear } = require('console')
+const { log } = require('console')
 const translator = require('./translator')
 
 const rl = readline.createInterface({
-   input: process.stdin,
-   output: process.output
+  input: process.stdin,
+  output: process.output,
 })
 
 clear()
-log(`\nНаправление перевода зависит от языка ввода.\nПоддерживается русский и английский языки.\nДля выхода попробуйте перевести букву "e".\n`)
+log(
+  `\nНаправление перевода зависит от языка ввода.\nПоддерживается русский и английский языки.\nДля выхода попробуйте перевести букву "e".\n`
+)
 
 rl.on('line', (cmd) => {
-   if (cmd === 'e' || cmd === 'е') { // одна из них кирилическая
-      clear()
-      rl.close()
-   } else {
-      clear()
-      log('please wait ...')
-      translator(cmd)
-         .then(data => {
-            clear()
-            log(`Оригинальный текст:\t${data.original}\nПеревод:\t\t${data.translated}\n`)
-         })
-         .catch(err => log('Ошибка: ', err))
-   }
+  if (cmd === 'e' || cmd === 'е') {
+    // одна из них кирилическая
+    clear()
+    rl.close()
+  } else {
+    clear()
+    log('please wait ...')
+    translator(cmd)
+      .then((data) => {
+        clear()
+        log(
+          `Оригинальный текст:\t${data.original}\nПеревод:\t\t${data.translated}\n`
+        )
+      })
+      .catch((err) => log('Ошибка: ', err))
+  }
 })

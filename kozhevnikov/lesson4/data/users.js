daaeff6299758deadaead7e665c5cf257ed74c4c
KostasKA2020 <konstakozh@yandex.ru>
2020-10-23 17:54:30 +0400

    lesson4

---
+++ kozhevnikov/lesson4/data/users.js
@@ -0,0 +1,18 @@
+module.exports = {
+    anna: {
+        username: 'Anna',
+        age: 25,
+        skills: [
+            'JS',
+            'React.js',
+        ],
+    },
+    ivan: {
+        username: 'Ivan',
+        skills: [
+            'JS',
+            'Node.js',
+            'React.js',
+        ],
+    },
+};
--- kozhevnikov/lesson4/express.js
+++ kozhevnikov/lesson4/express.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const hbs = require('express-handlebars')
+const cookieParser = require('cookie-parser')
 const path = require('path')
 
+const users = require('./data/users')
 
 const app = express()
 
 app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cookieParser())
+app.use(express.static('public'))
 
 app.engine('hbs', hbs({
     extname: 'hbs',
@@ -13,18 +18,39 @@ app.engine('hbs', hbs({
     layoutsDir: path.join(__dirname, 'views', 'layouts'),
     partialsDir: path.join(__dirname, 'views', 'partials'),
 }))
-app.set('view enjine', 'hbs')
+app.set('view engine', 'hbs')
 
 app.use((req, res, next) => {
+    console.log('Middleware 1')
+    // console.log(req)
+    // console.log(res)
+    next()
+})
+
+app.use('/users', (req, res, next) => {
+    console.log('Middleware 2')
+    next()
+})
+
+app.all('/users', (req, res, next) => {
+    console.log('Middleware 3')
+    next()
+})
+
+//Работа с заголовками
+app.use((req, res, next) => {
     console.log('Middleware 4')
+    // console.log(req.headers)
+   
     if(req.headers.test === '1234'){
         req.test = 'Передан пользовательский заголовок test!'
     }
     next()
 })
 
+
 app.get('/', (req, res) => {
-    res.status(200).send('Hello, express.js')
+    res.status(200).send('Hello, express.js!')
 })
 
 app.get('/users', (req, res) => {
@@ -33,19 +59,59 @@ app.get('/users', (req, res) => {
     } else {
         console.log('Заголовок test не был передан')
     }
-    req.render('users', {layout: 'default'})
+    // res.status(200).send('Users!')
+
+    res.render('users', {users})
 })
 
 app.post('/users', (req, res) => {
+   console.log(req.body)
     res.status(200).send('Post!')
 })
 
-app.get('/users', (req, res) => {
-    res.status(200).send('Users!')
+app.get('/users/:username', (req, res) => {
+    const {username} = req.params
+
+    const user = users[username]
+
+    if(!user){
+        res.status(404).render('error')
+    }
+
+    res.render('user', {user})
 })
 
+app.get('/config', (req, res) => {
+    res.render('config')
+ })
+
+ app.post('/config', (req, res) => {
+     console.log(req.body)
+     const {param1} = req.body
+     console.log(+param1)
+    res.redirect('/config')
+ })
+
+ app.get('/cookie/get', (req, res) => {
+     console.log(req.cookies)
+     res.send(JSON.stringify(req.cookies))
+ })
+
+ app.get('/cookie/set', (req, res) => {
+    res.cookie('count', Math.floor(Math.random() * 10))
+    res.redirect('/cookie/get')
+})
+
+//Вариант 1
+// app.use((req, res) => {
+//     res.status(404).render('error')
+// })
+
+//Вариант 2
+app.get('*', (req, res) => {
+    res.status(404).render('error')
+})
 
 app.listen(4000, () => {
-    console.log('http:/localhost:4000')
+    console.log('http://localhost:4000')
 })
-

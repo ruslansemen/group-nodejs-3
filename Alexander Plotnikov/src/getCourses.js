7cbff66c4cef479894186776ba83063ff131284c
Alexander Plotnikov <lotos_toi@mail.ru>
2020-10-26 08:51:11 +1000

    pars course gk

--- Alexander Plotnikov/src/getCourses.js
+++ Alexander Plotnikov/src/getCourses.js
@@ -2,10 +2,16 @@ const PORT = 4000
 const cheerio = require('cheerio')
 const link = 'https://geekbrains.ru/courses?tab=cour-new#cour-new'
 const fetch = require('node-fetch')
+const  parse5  =  require ( 'parse5' ) ;
 
 module.exports = async () => {
   let res = await fetch(link)
   let body = await res.text()
+
+  const d =parse5.parse(body)
+
+  //console.log(d.childNodes[1].childNodes[1].childNodes[0].childNodes[8]);
+
   let $ = cheerio.load(body)
   let number = $('body').find('a.gb-course-card__wrapper').length
   let courses = $('div.gb-course-card').slice(0, number - 1)

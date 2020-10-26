b061025622888346a920a5add1bebb4457d12912
KostasKA2020 <konstakozh@yandex.ru>
2020-10-26 13:18:29 +0400

    lesson4

--- kozhevnikov/lesson4/data/links.js
+++ kozhevnikov/lesson4/data/links.js
@@ -1,18 +1,5 @@
 module.exports = {
     rossaprimavera: {
         mediaName: 'rossaprimavera',
-        age: 25,
-        skills: [
-            'JS',
-            'React.js',
-        ],
     },
-    // ivan: {
-    //     username: 'Ivan',
-    //     skills: [
-    //         'JS',
-    //         'Node.js',
-    //         'React.js',
-    //     ],
-    // },
 };
